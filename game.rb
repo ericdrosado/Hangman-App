d4afc7beac13f9813b5bdadbdf9ed49aa12b921c
Eric D Rosado <rosadoericd@gmail.com>
2017-06-15 10:03:31 -0500

    Word and letter variables are now local in game_play method

--- game.rb
+++ game.rb
@@ -3,8 +3,6 @@ require_relative 'io_handler'
 
 class Game
 
-  attr_reader :word, :letter
-
   def initialize game_view, io_handler, prompter, validator
     @game_view = game_view
     @io_handler = io_handler
@@ -15,15 +13,15 @@ class Game
   def play_game
     @io_handler.printer(@prompter.game_greeting)
     @io_handler.printer(@prompter.prompt_for_word)
-    @word = @validator.validate_selection(@prompter.prompt_for_word, 'is_a_word?')
-    @word = @word.upcase
+    word = @validator.validate_selection(@prompter.prompt_for_word, 'is_a_word?')
+    word = word.upcase
     @game_view.clear_view
     @io_handler.printer(@prompter.prompt_for_letter)
-    @io_handler.printer(@game_view.word_blank(@word))
-    @letter = @validator.validate_selection(@prompter.prompt_for_letter, 'is_a_letter?')
-    @letter = @letter.downcase
-    @game_view.guessed_letters_view(@letter)
-    @io_handler.printer(@game_view.word_blank(@game_view.letter_swap(@word, @letter)))
+    @io_handler.printer(@game_view.word_blank(word))
+    letter = @validator.validate_selection(@prompter.prompt_for_letter, 'is_a_letter?')
+    letter = letter.downcase
+    @game_view.guessed_letters_view(letter)
+    @io_handler.printer(@game_view.word_blank(@game_view.letter_swap(word, letter)))
     @io_handler.printer(@game_view.guessed_letters)
   end
   

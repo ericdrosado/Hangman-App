7099c2b50aab4091529b9d3f073981949c4a9318
Eric D Rosado <rosadoericd@gmail.com>
2017-06-27 11:42:11 -0500

    Changed guess_counter to incorrect_guess_counter

--- game.rb
+++ game.rb
@@ -23,7 +23,7 @@ class Game
   end
 
   def player_two_guess_word word
-    guess_counter = 0
+    incorrect_guess_counter = 0
     end_of_game = false
     @io_handler.print(@game_view.body_array)
     while ! end_of_game
@@ -32,7 +32,7 @@ class Game
         letter = @validator.validate_selection(@prompter.prompt_for_letter, 'is_a_letter?').downcase
         @io_handler.print(@validator.validate_body_part_removal(@validator.is_letter_present_in_word?(word, letter)))
         @io_handler.print(@validator.is_guess_correct?(@validator.is_letter_present_in_word?(word, letter)))
-        guess_counter = @validator.validate_guess_counter_count(@validator.is_letter_present_in_word?(word, letter), guess_counter)
+        incorrect_guess_counter = @validator.validate_guess_counter_count(@validator.is_letter_present_in_word?(word, letter), incorrect_guess_counter)
         @game_view.guessed_letters_view(letter)
         word = @game_view.swap_letters_by_case(word, letter)
         @io_handler.print(@game_view.blank_word(word))
@@ -41,10 +41,10 @@ class Game
         word_guess = @io_handler.get_input
         @io_handler.print(@validator.validate_word_guess_for_body_part_removal(word, word_guess))
         @io_handler.print(@validator.is_guess_correct?(@validator.correct_word?(word, word_guess)))
-        guess_counter = @validator.validate_guess_counter_count(@validator.validate_word_guess(word, word_guess), guess_counter)
-        end_of_game = @validator.end_of_game?(word, word_guess, guess_counter)
+        incorrect_guess_counter = @validator.validate_guess_counter_count(@validator.validate_word_guess(word, word_guess), incorrect_guess_counter)
+        end_of_game = @validator.end_of_game?(word, word_guess, incorrect_guess_counter)
     end
-    @validator.validate_win_or_loss(guess_counter)
+    @validator.validate_win_or_loss(incorrect_guess_counter)
   end
 
 end
